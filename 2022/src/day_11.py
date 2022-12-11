587e7ae381f9d748d56059c6abf9f0c770329714
savarin <ezzeri.esa@gmail.com>
2022-12-11 13:11:47 -0800

    first pass Day 11 part 2

--- 2022/src/day_11.py
+++ 2022/src/day_11.py
@@ -1,4 +1,4 @@
-from typing import Callable, List, Tuple
+from typing import Callable, Dict, List, Optional, Tuple
 import dataclasses
 
 
@@ -7,8 +7,12 @@ def convert_to_action(
     operation: Callable[[int], int],
     divisor: int,
     receivers: Tuple[int, int],
+    base: Optional[int],
 ) -> Tuple[int, int]:
-    level = operation(level) // 3
+    if base is None:
+        level = operation(level) // 3
+    else:
+        level = operation(level) % base
 
     if level % divisor == 0:
         return level, receivers[0]
@@ -26,20 +30,20 @@ class Agent:
     def __post_init__(self) -> None:
         self.counter: int = 0
 
-    def play(self) -> Tuple[int, int]:
+    def play(self, base: Optional[int] = None) -> Tuple[int, int]:
         self.counter += 1
         return convert_to_action(
-            self.levels.pop(0), self.operation, self.divisor, self.receivers
+            self.levels.pop(0), self.operation, self.divisor, self.receivers, base
         )
 
 
-def test_agent_play():
+def test_agent_play() -> None:
     agents = {}
     agents[0] = Agent(
         operation=lambda x: x * 19, divisor=23, receivers=(2, 3), levels=[79, 98]
     )
     agents[1] = Agent(
-        operation=lambda x: x + 6, divisor=19, receivers=(2, 0), levels=[54, 65, 75, 75]
+        operation=lambda x: x + 6, divisor=19, receivers=(2, 0), levels=[54, 65, 75, 74]
     )
     agents[2] = Agent(
         operation=lambda x: x * x, divisor=13, receivers=(1, 3), levels=[79, 60, 97]
@@ -53,7 +57,7 @@ def test_agent_play():
             level, recipient = agents[i].play()
             agents[recipient].levels.append(level)
 
-    assert agents[0].levels == [20, 23, 27, 27]
+    assert agents[0].levels == [20, 23, 27, 26]
     assert agents[1].levels == [2080, 25, 167, 207, 401, 1046]
     assert agents[2].levels == []
     assert agents[3].levels == []
@@ -64,14 +68,12 @@ def test_agent_play():
             agents[recipient].levels.append(level)
 
     assert agents[0].levels == [695, 10, 71, 135, 350]
-    assert agents[1].levels == [43, 49, 58, 58, 362]
+    assert agents[1].levels == [43, 49, 58, 55, 362]
     assert agents[2].levels == []
     assert agents[3].levels == []
 
 
-if __name__ == "__main__":
-    test_agent_play()
-
+def create_agents() -> Dict[int, Agent]:
     agents = {}
     agents[0] = Agent(
         operation=lambda x: x * 3,
@@ -110,10 +112,33 @@ if __name__ == "__main__":
         operation=lambda x: x + 5, divisor=17, receivers=(3, 5), levels=[81, 58]
     )
 
+    return agents
+
+
+if __name__ == "__main__":
+    test_agent_play()
+
+    agents = create_agents()
+
     for _ in range(20):
         for i in range(8):
             while len(agents[i].levels):
                 level, recipient = agents[i].play()
+                agents[recipient].levels.append(level)
+
+    targets = sorted([agents[i].counter for i in range(8)], reverse=True)[:2]
+    print(targets[0] * targets[1])
+
+    agents = create_agents()
+    base = 1
+
+    for i in range(8):
+        base *= agents[i].divisor
+
+    for _ in range(10000):
+        for i in range(8):
+            while len(agents[i].levels):
+                level, recipient = agents[i].play(base)
                 agents[recipient].levels.append(level)
 
     targets = sorted([agents[i].counter for i in range(8)], reverse=True)[:2]
