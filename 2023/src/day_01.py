a704ed460b4d1dd749686eb7b171143dda8ff87e
Ezzeri Esa <ezzeri.esa@gmail.com>
2024-01-05 21:41:07 -0800

    first pass 2023 Day 1 part 2

--- 2023/src/day_01.py
+++ 2023/src/day_01.py
@@ -1,4 +1,4 @@
-from typing import Iterable, Optional
+from typing import List, Iterable, Optional
 
 from . import helpers
 
@@ -19,7 +19,7 @@ def get_last_number(string: str) -> Optional[str]:
     return None
 
 
-def combine_numbers(line: str) -> int:
+def combine_numbers_simple(line: str) -> int:
     first_number = get_first_number(line)
     last_number = get_last_number(line)
     number = (first_number or "") + (last_number or "")
@@ -27,15 +27,85 @@ def combine_numbers(line: str) -> int:
     return int(number) if number != "" else 0
 
 
-def get_calibration_values(lines: Iterable[str]) -> int:
+def get_calibration_values_simple(lines: Iterable[str]) -> int:
     total = 0
 
     for line in lines:
-        number = combine_numbers(line)
-        total += int(number)
+        total += combine_numbers_simple(line)
+
+    return total
+
+
+def is_substring_equal_word(string: str, word: str, index: int) -> Optional[str]:
+    if index + len(word) > len(string):
+        return None
+
+    elif string[index : index + len(word)] == word:
+        return word
+
+    return None
+
+
+def get_numbers(line: str) -> List[str]:
+    numbers = []
+
+    for i, char in enumerate(line):
+        if char.isdigit():
+            numbers.append(char)
+
+        elif char == "o" and is_substring_equal_word(line, "ne", i + 1):
+            numbers.append("1")
+
+        elif char == "t":
+            if is_substring_equal_word(line, "wo", i + 1):
+                numbers.append("2")
+
+            elif is_substring_equal_word(line, "hree", i + 1):
+                numbers.append("3")
+
+        elif char == "f":
+            if is_substring_equal_word(line, "our", i + 1):
+                numbers.append("4")
+
+            elif is_substring_equal_word(line, "ive", i + 1):
+                numbers.append("5")
+
+        elif char == "s":
+            if is_substring_equal_word(line, "ix", i + 1):
+                numbers.append("6")
+
+            elif is_substring_equal_word(line, "even", i + 1):
+                numbers.append("7")
+
+        elif char == "e" and is_substring_equal_word(line, "ight", i + 1):
+            numbers.append("8")
+
+        elif char == "n" and is_substring_equal_word(line, "ine", i + 1):
+            numbers.append("9")
+
+    return numbers
+
+
+def combine_numbers_composite(line: str) -> int:
+    numbers = get_numbers(line)
+
+    if len(numbers) == 0:
+        return 0
+
+    return int(numbers[0] + numbers[-1])
+
+
+def get_calibration_values_composite(lines: Iterable[str]) -> int:
+    total = 0
+
+    for line in lines:
+        total += combine_numbers_composite(line)
 
     return total
 
 
 if __name__ == "__main__":
-    print(get_calibration_values(helpers.generate_lines("2023/data/day_01.txt")))
+    filename = "2023/data/day_01.txt"
+
+    print(get_calibration_values_simple(helpers.generate_lines(filename)))
+    print(get_calibration_values_composite(helpers.generate_lines(filename)))
--- 2023/src/test_day_01.py
+++ 2023/src/test_day_01.py
@@ -1,11 +1,30 @@
 from . import day_01
 
 
-def test_get_calibration_values() -> None:
+def test_get_calibration_values_simple() -> None:
     document = """\
 1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet"""
 
-    assert day_01.get_calibration_values(document.split("\n")) == 142
+    assert day_01.get_calibration_values_simple(document.split("\n")) == 142
+
+
+def test_get_numbers() -> None:
+    assert day_01.get_numbers("two1nine") == ["2", "1", "9"]
+    assert day_01.get_numbers("eightwothree") == ["8", "2", "3"]
+    assert day_01.get_numbers("abcone2threexyz") == ["1", "2", "3"]
+
+
+def test_get_calibration_values_composite() -> None:
+    document = """\
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"""
+
+    assert day_01.get_calibration_values_composite(document.split("\n")) == 281
