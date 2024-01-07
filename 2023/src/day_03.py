7c0e6dc131a675d7cb8859ebcde5eecc9031b2a6
Ezzeri Esa <ezzeri.esa@gmail.com>
2024-01-07 04:04:58 -0800

    first pass 2023 Day 3 part 1

---
+++ 2023/src/day_03.py
@@ -0,0 +1,88 @@
+from typing import DefaultDict, Iterable, List, Tuple
+import collections
+
+import helpers
+
+
+def get_neighbors(
+    location: Tuple[int, int], max_row_index: int, max_column_index: int
+) -> List[Tuple[int, int]]:
+    neighbors = []
+
+    for neighbor in [
+        (location[0] + 1, location[1]),
+        (location[0] - 1, location[1]),
+        (location[0], location[1] + 1),
+        (location[0], location[1] - 1),
+        (location[0] + 1, location[1] + 1),
+        (location[0] + 1, location[1] - 1),
+        (location[0] - 1, location[1] + 1),
+        (location[0] - 1, location[1] - 1),
+    ]:
+        if neighbor[0] <= max_row_index and neighbor[1] <= max_column_index:
+            neighbors.append(neighbor)
+
+    return neighbors
+
+
+def get_part_numbers_sum(lines: Iterable[str]) -> int:
+    digit_chars = ""
+    digit_locations = set()
+    digits = {}
+    digit_count: DefaultDict[int, int] = collections.defaultdict(int)
+
+    symbol_locations = []
+
+    max_row_index, max_column_index = 0, 0
+
+    for i, line in enumerate(lines):
+        if max_row_index < i:
+            max_row_index = i
+
+        for j, char in enumerate(line):
+            if max_column_index < j:
+                max_column_index = j
+
+            if char.isdigit():
+                digit_chars += char
+                digit_locations.add((i, j))
+                continue
+
+            if digit_chars != "":
+                digit = int(digit_chars)
+                digits[(digit, digit_count[digit])] = digit_locations
+                digit_count[digit] += 1
+
+                digit_chars = ""
+                digit_locations = set()
+
+            if char == ".":
+                continue
+
+            symbol_locations.append((i, j))
+
+    part_numbers = []
+
+    for symbol_location in symbol_locations:
+        for neighbor in get_neighbors(symbol_location, max_row_index, max_column_index):
+            is_digit_adjacent = False
+
+            for digit_pair, digit_locations in digits.items():
+                if neighbor in digit_locations:
+                    is_digit_adjacent = True
+                    break
+
+            if is_digit_adjacent:
+                part_numbers.append(digit_pair[0])
+                del digits[digit_pair]
+
+            if len(digits) == 0:
+                break
+
+    return sum(part_numbers)
+
+
+if __name__ == "__main__":
+    lines = helpers.generate_lines("2023/data/day_03.txt")
+
+    print(get_part_numbers_sum(lines))
---
+++ 2023/src/test_day_03.py
@@ -0,0 +1,17 @@
+import day_03
+
+
+def test_get_calibration_values_simple() -> None:
+    document = """\
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."""
+
+    assert day_03.get_part_numbers_sum(document.split("\n")) == 4361
